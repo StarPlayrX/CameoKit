f290ffe456b06b856e5402ae0767664c0597198c
Todd Bruss <admin@starplayrx.com>
2021-06-18 13:39:00 -0400

    mid cylce

--- Sources/CameoKit/Sirius Channels/Channels.swift
+++ Sources/CameoKit/Sirius Channels/Channels.swift
@@ -75,34 +75,40 @@ internal func processChannels(result: PostReturnTuple) -> (success: Bool, messag
                         
                         let chNumber = Int(channelNumber)
                         switch chNumber {
-                            case 20,18,19,22,23,24,29,30,31,32,38,42,104,176,700,711,717,726,730,743:
+                            case 20,18,19,22,23,24,29,30,31,32,38,42,75,104,176,333,700,711,717:
                                 category = "Artists"
                             case 4,11,769:
                                 category = "Pop"
-                            case 7,8,27,28,301,302:
+                            case 7,8,12,17,27,28,35,301:
                                 category = "Rock"
-                            case 13:
+                            case 13,718:
                                 category = "Dance/Electronic"
-                            case 9,21,33,34,36,173:
+                            case 14,9,21,33,34,36,173:
                                 category = "Alternative"
-                            case 12,35,37,39,40,41:
+                            case 37,39,40,41,714:
                                 category = "Metal"
                             case 5,6,701,703,776:
                                 category = "Oldies"
                             case 314,712,713:
                                 category = "Punk"
+                            case 48,68,290,330:
+                                category = "Love Songs"
+                            case 302:
+                                category = "Family"
                             case 165:
                                 category = "Canada Music"
                             case 172:
                                 category = "Sports Talk"
                             case 171:
                                 category = "Country"
-                            case 141, 142, 706, 721:
+                            case 141, 142, 706:
                                 category = "Jazz/Standards/Classical"
                             case 169:
                                 category = "Canada Talk"
                             case 152, 158:
                                 category = "Latin Music"
+                            case 708,719,721,726,730,743:
+                                category = "X Info"
                             default:
                                 _ = category
                             //category = category
@@ -157,154 +163,6 @@ internal func processChannels(result: PostReturnTuple) -> (success: Bool, messag
                 
                 return (success: success, message: message, data: ChannelDict, recordCategories)
             }
-        }
-    }
-    
-    return (success: success, message: message, data: Dictionary<String, Any>(), categories: recordCategories)
-}
-
-internal func Channels2() -> ChannelsTuple {
-    var recordCategories = Array<String>()
-    
-    var success : Bool = false
-    var message : String = "Something's not right."
-    let endpoint = "https://\(playerDomain)/rest/v2/experience/modules/get?cacheBuster=1595033362033"
-    let method = "channels"
-    let request =  ["moduleList":["modules":[["moduleArea":"Discovery","moduleType":"ChannelListing","moduleRequest":["resultTemplate":""]]]]] as Dictionary
-    
-    let result = PostSync(request: request, endpoint: endpoint, method: method )
-    
-    if (result.response?.statusCode) == 403 {
-        success = false
-        message = "Too many incorrect logins, your Sat Radio provider has blocked your IP for 24 hours."
-    }
-    
-    if result.success {
-        let result = result.data as NSDictionary
-        if let r = result.value(forKeyPath: "ModuleListResponse.moduleList.modules") {
-            let m = r as? NSArray
-            let o = m![0] as! NSDictionary
-            let d = o.value( forKeyPath: "moduleResponse.contentData.channelListing.channels") as! NSArray
-            
-            var ChannelDict : Dictionary = Dictionary<String, Any>()
-            var ChannelIdDict : Dictionary = Dictionary<String, Any>()
-            
-            
-            for i in d {
-                if let dict = i as? NSDictionary,
-                    let channelId = dict.value( forKeyPath: "channelId") as? String {
-                    let categories = dict.value( forKeyPath: "categories.categories") as? NSArray
-                    
-                    if let cats = categories?.firstObject as? NSDictionary,
-                        let channelNumber = dict.value( forKeyPath: "channelNumber") as? String,
-                        var category = cats.value( forKeyPath: "name") as? String {
-                        
-                        switch category {
-                            case "MLB Play-by-Play":
-                                category = "MLB"
-                            case "NBA Play-by-Play":
-                                category = "NBA"
-                            case "NFL Play-by-Play":
-                                category = "NFL"
-                            case "NHL Play-by-Play":
-                                category = "NHL"
-                            case "Sports Play-by-Play":
-                                category = "Play-by-Play"
-                            default:
-                                _ = category
-                        }
-                        
-                        let chNumber = Int(channelNumber)
-                        switch chNumber {
-                            case 20,18,19,22,23,24,31,29,30,38,176,700,711:
-                                category = "Artists"
-                            case 11,12:
-                                category = "Pop"
-                            case 4,7,8,28,301,302:
-                                category = "Rock"
-                            case 13:
-                                category = "Dance/Electronic"
-                            case 9,21,33,34,35,36,173,359,714,758:
-                                category = "Alternative"
-                            case 37,39,40,41:
-                                category = "Heavy Metal"
-                            case 5,6,701,703,776:
-                                category = "Oldies"
-                            case 314,712,713:
-                                category = "Punk"
-                            case 165,169:
-                                category = "Canadian"
-                            case 172:
-                                category = "Sports"
-                            case 171:
-                                category = "Country"
-                            case 141, 142, 706:
-                                category = "Jazz/Standards/Classical"
-                            case 152, 158:
-                                category = "Latino"
-                            default:
-                                _ = category
-                            //category = category
-                        }
-                        
-                        // append it to the categorieshit
-                        if !recordCategories.contains(category) {
-                            recordCategories.append(category)
-                        }
-                        
-                        var mediumImage = ""
-                        
-                        if let images = dict.value( forKeyPath: "images.images") as? NSArray,
-                            let name = dict.value( forKeyPath: "name") as? String {
-                            
-                            let a = 4 //low
-                            let b = 8 //high
-                            
-                            for img in images.reversed()[a...b] {
-                                
-                                if let g = img as? NSDictionary, let height = g["height"] as? Int, let name = g["name"] as? String {
-                                    
-                                    if height == 720 && name == "color channel logo (on dark)" {
-                                        if let mi = g["url"] as? String {
-                                            mediumImage = mi
-                                            
-                                            break
-                                        }
-                                    }
-                                }
-                            }
-                            
-                            let cl = [ "channelId": channelId, "channelNumber": channelNumber, "name": name,
-                                       "mediumImage": mediumImage, "category": category, "preset": false ] as [String : Any]
-                            
-                            
-                            let ids = ["channelNumber": channelNumber] as [String : String]
-                            
-                           
-                            ChannelDict[channelNumber] = cl
-                            ChannelIdDict[channelId] = ids
-                            
-                        }
-                        
-                        
-                    }
-                }
-            }
-            
-            userX.channels = ChannelDict
-            userX.ids = ChannelIdDict
-            
-            if !userX.channels.isEmpty {
-                
-                UserDefaults.standard.set(ChannelDict, forKey: "channels")
-                UserDefaults.standard.set(ChannelIdDict, forKey: "ids")
-                
-                success = true
-                message = "Read the channels in."
-                
-                return (success: success, message: message, data: ChannelDict, recordCategories)
-            }
-            
         }
     }
     
